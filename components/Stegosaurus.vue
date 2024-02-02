72cf6762c73c99d1a23441b580effe11272a61fb
KawabeKousuke <144089852+KawabeKousuke@users.noreply.github.com>
2024-02-02 07:20:47 +0000

    ステゴサウルス

---
+++ components/Stegosaurus.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>ステゴサウルス</h2>
+                <img src="images/ステゴサウルスいらすとや.png" alt="ステゴサウルス" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>説明</h3>
+                <p>全長:6~8m</p>
+                <p>体重:2~4トン</p>
+                <p>後期ジュラ紀の北米大陸に生息していた植物食恐竜です。</p>
+                <h4>特徴</h4>
+                <p>背中に二列に並んだ骨板と、尻尾にスパイクのような４本のトゲが並んでいます。</p>
+                <p>頭が異常に小さく、口には先のとがった角質の口ばしがあり、柔らかい植物を食べていたと考えられます。</p>
+            </div>
+        </div>
+    </div>
+</template>

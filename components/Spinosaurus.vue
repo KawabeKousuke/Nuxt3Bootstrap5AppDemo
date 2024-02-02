7c0156942cf8e0ee7d1c325a38a257f6d6fc6563
KawabeKousuke <144089852+KawabeKousuke@users.noreply.github.com>
2024-02-02 07:26:00 +0000

    スピノサウルス

---
+++ components/Spinosaurus.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>スピノサウルス</h2>
+                <img src="images/スピノサウルスいらすとや.png" alt="スピノサウルス" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>説明</h3>
+                <p>体長:15m</p>
+                <p>体重:7トン</p>
+                <p></p>
+                <h4>特徴</h4>
+                <p>背中に二列に並んだ骨板と、尻尾にスパイクのような４本のトゲが並んでいます。</p>
+                <p>頭が異常に小さく、口には先のとがった角質の口ばしがあり、柔らかい植物を食べていたと考えられます。</p>
+            </div>
+        </div>
+    </div>
+</template>

a1bf31c9e337bb774f4c52b5d75a1c207b40b8b6
KawabeKousuke <144089852+KawabeKousuke@users.noreply.github.com>
2024-02-02 07:06:09 +0000

    ヴェロキラプトル

---
+++ components/Velociraptor.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>ヴェロキラプトル</h2>
+                <img src="images/ヴェロキラプトルいらすとや.png" alt="ヴェロキラプトル" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>説明</h3>
+                <p>全長:1.5~2m</p>
+                <p>体重:16kg</p>
+                <p>中生代白亜紀後期のアジアに生息する小型の肉食恐竜です。</p>
+                <h4>特徴</h4>
+                <p>体全体に比べて脳と目が大きかったため、最も知能の高い恐竜と言われています。</p>
+                <p>鋭い歯と爪をもち、足が速く、群れで狩りを行ったと考えられます。</p>
+            </div>
+        </div>
+    </div>
+</template>

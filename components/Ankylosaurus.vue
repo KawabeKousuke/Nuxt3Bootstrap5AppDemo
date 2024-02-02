2cdbaaeb688b24f2c9f7c25b74616bea78a579ba
KawabeKousuke <144089852+KawabeKousuke@users.noreply.github.com>
2024-02-02 06:54:07 +0000

    アンキロサウルス

---
+++ components/Ankylosaurus.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>アンキロサウルス</h2>
+                <img src="images/アンキロサウルスいらすとや.png" alt="アンキロサウルス" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>説明</h3>
+                <p>全長:7~11m</p>
+                <p>体重:6~8トン</p>
+                <p>北アメリカに生息していた白亜紀後期の植物食恐竜です。</p>
+                <h4>特徴</h4>
+                <p>頭と背中全体が多角形の堅い骨板の重なってできた鎧でおおわれている。そのため「鎧竜」とも呼ばれています。</p>
+                <p>尻尾には大きな骨塊を持ち、外敵に対してこの尾を武器にして戦ったと推測しています。</p>
+            </div>
+        </div>
+    </div>
+</template>

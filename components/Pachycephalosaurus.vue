87177092c4135384f7a160ecf6a7457c85a21c21
KawabeKousuke <144089852+KawabeKousuke@users.noreply.github.com>
2024-02-03 06:37:02 +0000

    パキケファロサウルス

---
+++ components/Pachycephalosaurus.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>パキケファロサウルス</h2>
+                <img src="images/パキケファロサウルスいらすとや.png" alt="パキケファロサウルス" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>説明</h3>
+                <p>体長:4~5</p>
+                <p>体重:250~800kg</p>
+                <p>白亜紀後期の北アメリカに生息していた植物食恐竜です。</p>
+                <h4>特徴</h4>
+                <p>分厚い頭骨を持っていて、それを活かした頭突き攻撃をします。</p>
+            </div>
+        </div>
+    </div>
+</template>

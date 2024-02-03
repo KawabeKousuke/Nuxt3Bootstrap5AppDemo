1536a60fc2e1daa6c00f70d1de844bc61f89bab2
KawabeKousuke <144089852+KawabeKousuke@users.noreply.github.com>
2024-02-03 07:32:15 +0000

    ブラキオサウルス

---
+++ components/Brachiosaurus.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h2>ブラキオサウルス</h2>
+                <img src="images/ブラキオサウルスいらすとや.png" alt="ブラキオサウルス" style="width: 700px; height: auto;">
+            </div>
+            <div class="col-md-5">
+                <h3>説明</h3>
+                <p>体長:25m</p>
+                <p>体重:50トン</p>
+                <p>中生代ジュラ紀後期の北米とアフリカに生息していた草食恐竜です。</p>
+                <h4>特徴</h4>
+                <p></p>
+                <p></p>
+            </div>
+        </div>
+    </div>
+</template>

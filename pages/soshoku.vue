3eae26feab656b3eaa3e36ee1fefb76426dac1c2
KawabeKousuke <144089852+KawabeKousuke@users.noreply.github.com>
2024-02-03 13:14:45 +0000

    草食恐竜

--- pages/soshoku.vue
+++ pages/soshoku.vue
@@ -1,7 +1,12 @@
+import Brachiosaurus from '~/components/Brachiosaurus.vue';
 <template>
     <div class="container">
       <h1 class="text-danger">恐竜サイト</h1>
       <h2>このサイトは恐竜のことが詳しく書かれているサイトです。</h2>
-     
+      <Triceratops />
+      <Stegosaurus />
+      <Ankylosaurus />
+      <Pachycephalosaurus />
+      <Brachiosaurus />  
     </div>
   </template>
